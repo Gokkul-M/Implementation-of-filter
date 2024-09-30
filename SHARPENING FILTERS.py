2c68c4942e72f45beddad4b544d5bf0a2c6c70d7
Gokkul.M <144870543+Gokkul-M@users.noreply.github.com>
2024-09-30 15:55:59 +0530

    Update SHARPENING FILTERS.py

--- SHARPENING FILTERS.py
+++ SHARPENING FILTERS.py
@@ -5,29 +5,43 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-image1=cv2.imread("red.jpg")
-image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
-kernel=np.ones((11,11),np.float32)/121
-image3=cv2.filter2D(image2,-1,kernel)
-kernel2=np.array([[-1,-1,-1],])
-image3=cv2.filter2D(image2,)
-plt.figure(figsize=(,8))
-plt.subplot()
+image1 = cv2.imread(r"C:\Users\admin\Downloads\image1.jpeg")
+image2 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB) 
+kernel = np.ones((11, 11), np.float32) / 121 
+blurred_image = cv2.filter2D(image2, -1, kernel)
+kernel2 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+sharpened_image = cv2.filter2D(image2, -1, kernel2) 
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 3, 1)
 plt.imshow(image2)
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,2,)
+plt.subplot(1, 3, 3)
+plt.imshow(sharpened_image)
+plt.title("Sharpened Image")
+plt.axis("off")
+plt.show()
 
 
 # In[5]:Using Laplacian Operator
 
-
-laplacian=cv2.Laplacian(image2,)
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+image1 = cv2.imread(r"C:\Users\admin\Downloads\image1.jpeg")
+image2 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB) 
+laplacian = cv2.Laplacian(image2, cv2.CV_64F)
 plt.figure(figsize=(8,8))
-plt.subplot()
+plt.subplot(1, 2, 1)
 plt.imshow(image2)
 plt.title("Original Image")
 plt.axis("off")
+plt.subplot(1, 2, 2)
+plt.imshow(laplacian, cmap='gray')
+plt.title("Laplacian Image")
+plt.axis("off")
+plt.show()
+
 
 
 
